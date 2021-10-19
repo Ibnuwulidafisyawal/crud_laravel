4015242b8cf5dc65d71d9c64a30dbd6dbcd024e5
Ibnuwulidafisyawal <ibnuwulfisyawal@gmail.com>
2021-10-19 21:35:57 +0700

    crud laravel

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Absensi Laravel </title>
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+  
+<div class="container">
+    <br>
+    <br>
+    @yield('content')
+</div>
+   
+</body>
+</html>
